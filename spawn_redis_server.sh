7956d48b4948dca4084a425ef63b288e9dac5a42
David Bousi <bousi.david@gmail.com>
2024-05-09 17:20:47 +0200

    sh update

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
-#
-# DON'T EDIT THIS!
-#
-# CodeCrafters uses this file to test your code. Don't make any changes here!
-#
-# DON'T EDIT THIS!
 exec cargo run \
     --quiet \
     --release \
-    --target-dir=/tmp/codecrafters-redis-target \
+    --target-dir=/tmp/redis-target \
     --manifest-path $(dirname $0)/Cargo.toml \
     -- "$@"
